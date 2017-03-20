47f46ec3859a362d072df690aa02200ca67c5bf0
Muhammad Sadiq Saeed <ss.sadiqsaeed@gmail.com>
2017-03-20 19:44:02 +0500

    Some additions

---
+++ src/testing/Test2.java
@@ -0,0 +1,11 @@
+package testing;
+
+/**
+ * Created by Muhammad Sadiq Saeed on 3/20/2017.
+ */
+public class Test2 {
+
+    public static void main(String[] args) {
+        System.out.println("Test 2....... ");
+    }
+}
